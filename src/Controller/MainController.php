72e312ec4be28fb1995815255f0316eebe558197
Andrew SASSOYE <andrew@sassoye.be>
2021-03-17 14:45:52 +0100

    init index route and template

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="app.index")
+     */
+    public function index() {
+        return $this->render("static/index.html.twig");
+    }
+}
