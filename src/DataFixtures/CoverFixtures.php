9a7401d1b4ba78ad61a9b108a33080502ebfbb29
Andrew SASSOYE <andrew@sassoye.be>
2021-08-03 22:06:03 +0200

    database: create Cover fixtures

---
+++ src/DataFixtures/CoverFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Cover;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+class CoverFixtures extends Fixture
+{
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $json = json_decode(file_get_contents('resources/covers.json'));
+
+        foreach ($json as $item) {
+            $element = new Cover();
+            $element
+                    ->setUrl(strval($item->url))
+                    ->setWidth($item->width)
+                    ->setHeight($item->height);
+
+            $manager->persist($element);
+            $this->addReference("genre".strval($item->id), $element);
+        }
+
+        $manager->flush();
+    }
+}
