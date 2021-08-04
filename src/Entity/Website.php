383748aa1d3293cecfc2b595f627e9cfadb0581f
Andrew SASSOYE <andrew@sassoye.be>
2021-08-04 19:03:41 +0200

    database: create Website entity + migration

---
+++ src/Entity/Website.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WebsiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WebsiteRepository::class)
+ */
+class Website
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $trusted;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategory(): ?int
+    {
+        return $this->category;
+    }
+
+    public function setCategory(int $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getTrusted(): ?bool
+    {
+        return $this->trusted;
+    }
+
+    public function setTrusted(bool $trusted): self
+    {
+        $this->trusted = $trusted;
+
+        return $this;
+    }
+}
