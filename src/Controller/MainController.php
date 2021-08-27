227e084d5aec84197ea944e496cfacbf77e4cac2
Andrew SASSOYE <andrew@sassoye.be>
2021-08-27 13:33:25 +0200

    add discover page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Game;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -12,8 +14,32 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="app.index")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render("static/index.html.twig");
+        return $this->redirectToRoute("app.discover");
+    }
+
+    /**
+     * @Route("/discover", name="app.discover")
+     */
+    public function discover(): Response
+    {
+        $gameR = $this->getDoctrine()->getRepository(Game::class);
+
+        $popular = $gameR->popular();
+        $favorites = $gameR->favorites();
+
+        return $this->render("pages/discover.html.twig", array(
+          "popular" => $popular,
+          "favorites" => $favorites,
+        ));
+    }
+
+    /**
+     * @Route("/about", name="app.about")
+     */
+    public function about(): Response
+    {
+        return $this->render("pages/about.html.twig");
     }
 }
--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -31,4 +31,52 @@ class GameRepository extends ServiceEntityRepository
 
         return $query->execute();
     }
+
+    public function popular(): array
+    {
+        $qb = $this->createQueryBuilder("g")
+          ->where('YEAR(g.releaseDate) > YEAR(NOW()) - 2')
+          ->andWhere('g.aggregatedRatingCount > 5')
+          ->andWhere('g.ratingCount > 50')
+          ->orderBy("g.totalRating", "DESC")
+          ->setMaxResults(18);
+
+        $query = $qb->getQuery();
+
+        return $query->execute();
+    }
+
+    public function favorites(): array
+    {
+        $qb = $this->createQueryBuilder("g")
+          ->where("g.slug = 'final-fantasy-ix'")
+          ->orWhere("g.slug = 'final-fantasy-xv'")
+          ->orWhere("g.slug = 'final-fantasy-x'")
+          ->orWhere("g.slug = 'final-fantasy-vii'")
+          ->orWhere("g.slug = 'final-fantasy-viii'")
+          ->orWhere("g.slug = 'final-fantasy-vii-remake'")
+          ->orWhere("g.slug = 'kingdom-hearts'")
+          ->orWhere("g.slug = 'kingdom-hearts-iii'")
+          ->orWhere("g.slug = 'kingdom-hearts-ii'")
+          ->orWhere("g.slug = 'the-last-guardian'")
+          ->orWhere("g.slug = 'ratchet-and-clank'")
+          ->orWhere("g.slug = 'hollow-knight'")
+          ->orWhere("g.slug = 'celeste'")
+          ->orWhere("g.slug = 'no-man-s-sky'")
+          ->orWhere("g.slug = 'assassin-s-creed-ii'")
+          ->orWhere("g.slug = 'god-of-war--1'")
+          ->orWhere("g.slug = 'the-elder-scrolls-v-skyrim-special-edition'")
+          ->orWhere("g.slug = 'fallout-4'")
+          ->orWhere("g.slug = 'portal'")
+          ->orWhere("g.slug = 'psychonauts'")
+          ->orWhere("g.slug = 'oddworld-abe-s-oddysee'")
+          ->orWhere("g.slug = 'crash-bandicoot-n-sane-trilogy'")
+          ->orWhere("g.slug = 'portal-2'")
+          ->orWhere("g.slug = 'inside'")
+          ->orderBy("g.totalRating", "DESC");
+
+        $query = $qb->getQuery();
+
+        return $query->execute();
+    }
 }
